dfbb5b8d7747e81f8d8b7f51131fb97d2b150715
Andi Kleen <ak@linux.intel.com>
2009-11-26 09:51:13 +0100

    Add GPL headers to trigger.c / client.c
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- yellow.c
+++ yellow.c
@@ -1,4 +1,20 @@
-/* Handle 'yellow bit' cache error threshold indications. */
+/* Copyright (C) 2009 Intel Corporation 
+   Author: Andi Kleen
+   Handle 'yellow bit' cache error threshold indications.
+
+   mcelog is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; version
+   2.
+
+   mcelog is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should find a copy of v2 of the GNU General Public License somewhere
+   on your Linux system; if not, write to the Free Software Foundation, 
+   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
 #define _GNU_SOURCE 1
 #include <stdio.h>
 #include <assert.h>
