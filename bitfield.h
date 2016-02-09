f3831561806044c8dd7665e531d7e36972ee10f7
Andi Kleen <ak@linux.intel.com>
2016-02-09 10:07:33 -0800

    More fixes for %ll instead of %L in *printf to support musl libc.
    Fixes https://github.com/andikleen/mcelog/issues/31
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- bitfield.h
+++ bitfield.h
@@ -16,10 +16,10 @@ struct numfield {
 #define FIELD(start_bit, name) { start_bit, name, NELE(name) }
 #define SBITFIELD(start_bit, string) { start_bit, ((char * [2]) { NULL, string }), 2 }
 
-#define NUMBER(start, end, name) { start, end, name, "%Lu", 0 }
-#define NUMBERFORCE(start, end, name) { start, end, name, "%Lu", 1 }
-#define HEXNUMBER(start, end, name) { start, end, name, "%Lx", 0 }
-#define HEXNUMBERFORCE(start, end, name) { start, end, name, "%Lx", 1 }
+#define NUMBER(start, end, name) { start, end, name, "%llu", 0 }
+#define NUMBERFORCE(start, end, name) { start, end, name, "%llu", 1 }
+#define HEXNUMBER(start, end, name) { start, end, name, "%llx", 0 }
+#define HEXNUMBERFORCE(start, end, name) { start, end, name, "%llx", 1 }
 
 void decode_bitfield(u64 status, struct field *fields);
 void decode_numfield(u64 status, struct numfield *fields);
