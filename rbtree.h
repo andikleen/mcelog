andikleen/mcelog

commit c18c8048f226244afbf73b85836967cae5cb7f99

Compress some fields in mempage.
Shrink mempage a bit to conserve memory.

We only need 32bit of counter.
Move other fields around to avoid padding.

Signed-off-by: Andi Kleen <ak@linux.intel.com>

rbtree.h
@@ -103,13 +103,12 @@ static inline struct page * rb_insert_page_cache(struct inode * inode,
 
 struct rb_node
 {
-	unsigned long  rb_parent_color;
 #define	RB_RED		0
 #define	RB_BLACK	1
 	struct rb_node *rb_right;
 	struct rb_node *rb_left;
-} __attribute__((aligned(sizeof(long))));
-    /* The alignment might seem pointless, but allegedly CRIS needs it */
+	unsigned char   rb_parent_color;
+} __attribute__((packed));
 
 struct rb_root
 {

memdb.c
@@ -274,7 +274,7 @@ static void dump_errtype(char *name, struct err_type *e, FILE *f, enum printflag
 	if (e->count || e->bucket.count || all)
 		fprintf(f, "%s:\n", name);
 	if (e->count || all) {
-		fprintf(f, "\t%lu total\n", e->count);
+		fprintf(f, "\t%u total\n", e->count);
 	}
 	if (bc->capacity && (e->bucket.count || all)) {
 		s = bucket_output(bc, &e->bucket);

memdb.h
@@ -3,7 +3,7 @@
 
 struct err_type {
 	struct leaky_bucket bucket;
-	unsigned long count;
+	unsigned count;
 };
 
 enum printflags {

page.c
@@ -45,11 +45,12 @@ enum { PAGE_ONLINE = 0, PAGE_OFFLINE = 1, PAGE_OFFLINE_FAILED = 2 };
 
 struct mempage { 
 	struct rb_node nd;
-	u64 addr;
-	struct err_type ce;
+	/* one char used by rb_node */
 	char offlined;
 	char triggered;
-	// 2(32bit)-6(64bit) bytes of padding to play with here
+	// 1(32bit)-5(64bit) bytes of padding to play with here
+	u64 addr;
+	struct err_type ce;
 };
 
 static struct rb_root mempage_root;
@@ -237,7 +238,7 @@ void dump_page_errors(FILE *f)
 		if (k++ == 0)
 			fprintf(f, "Per page corrected memory statistics:\n");
 		msg = bucket_output(&page_trigger_conf, &p->ce.bucket);
-		fprintf(f, "%llx: total %lu seen \"%s\" %s%s\n",
+		fprintf(f, "%llx: total %u seen \"%s\" %s%s\n",
 			p->addr,
 			p->ce.count,
 			msg,

commit 0a310abe7b0788ef100b5b2a8eb7c63f04bff1d7

Turn back rb_color field into unsigned long
Was broken in c18c8048f226244a

But of course we still need full 64bit for a full tree walk.

Fixes https://github.com/andikleen/mcelog/issues/65

Signed-off-by: Andi Kleen <ak@linux.intel.com>

rbtree.h
@@ -107,7 +107,7 @@ struct rb_node
 #define	RB_BLACK	1
 	struct rb_node *rb_right;
 	struct rb_node *rb_left;
-	unsigned char   rb_parent_color;
+	unsigned long   rb_parent_color;
 } __attribute__((packed));
 
 struct rb_root